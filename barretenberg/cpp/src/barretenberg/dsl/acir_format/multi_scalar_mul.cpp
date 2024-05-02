ad5e24a90930eb3e1db05498e0d6a1dd606ee5c8
benesjan <janbenes1234@gmail.com>
2024-05-02 15:06:43 +0000

    using efficient batch_mul

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/multi_scalar_mul.cpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/multi_scalar_mul.cpp
@@ -3,6 +3,7 @@
 #include "barretenberg/ecc/curves/bn254/fr.hpp"
 #include "barretenberg/ecc/curves/grumpkin/grumpkin.hpp"
 #include "barretenberg/plonk_honk_shared/arithmetization/gate_data.hpp"
+#include "barretenberg/stdlib/primitives/biggroup/biggroup.hpp"
 
 namespace acir_format {
 
@@ -12,32 +13,29 @@ template <typename Builder> void create_multi_scalar_mul_constraint(Builder& bui
     using cycle_scalar_ct = typename bb::stdlib::cycle_group<Builder>::cycle_scalar;
     using field_ct = bb::stdlib::field_t<Builder>;
 
-    // We initialize output_point as identity element/point at infinity
-    cycle_group_ct output_point(0, 0, true);
+    std::vector<cycle_group_ct> points;
+    std::vector<cycle_scalar_ct> scalars;
 
-    // We iterate over points and scalars in the input, we multiply each pair and then add the output point of the
-    // iteration to the output point
-    // Note: We iterate by 2 because each point and scalar is represented by 2 witness indices ([x, y] for point and
-    // [low, high] for scalar)
     for (size_t i = 0; i < input.points.size(); i += 2) {
-        // We instantiate the input point/variable base as `cycle_group_ct`
+        // Instantiate the input point/variable base as `cycle_group_ct`
         auto point_x = field_ct::from_witness_index(&builder, input.points[i]);
         auto point_y = field_ct::from_witness_index(&builder, input.points[i + 1]);
         cycle_group_ct input_point(point_x, point_y, false);
 
-        // We reconstruct the scalar from the low and high limbs
+        // Reconstruct the scalar from the low and high limbs
         field_ct scalar_low_as_field = field_ct::from_witness_index(&builder, input.scalars[i]);
         field_ct scalar_high_as_field = field_ct::from_witness_index(&builder, input.scalars[i + 1]);
         cycle_scalar_ct scalar(scalar_low_as_field, scalar_high_as_field);
 
-        // We multiply input point with the scalar to get the output point of this iteration
-        auto iteration_output_point = input_point * scalar;
-
-        // If this is not the first iteration, we add the result to the output point
-        output_point = output_point + iteration_output_point;
+        // Add the point and scalar to the vectors
+        points.push_back(input_point);
+        scalars.push_back(scalar);
     }
 
-    // Finally we add the constraints
+    // Call batch_mul to multiply the points and scalars and sum the results
+    auto output_point = cycle_group_ct::batch_mul(scalars, points);
+
+    // Add the constraints
     builder.assert_equal(output_point.x.get_witness_index(), input.out_point_x);
     builder.assert_equal(output_point.y.get_witness_index(), input.out_point_y);
 }
